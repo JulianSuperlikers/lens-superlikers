b59e4966cd5463bf7a7a220bcce7c6c183fd4a26
Andres Hortua <fandresh7@gmail.com>
2024-11-06 15:11:06 -0500

    change stringuuid to uid

--- src/controllers/veryfi.controller.js
+++ src/controllers/veryfi.controller.js
@@ -26,7 +26,7 @@ export async function processDocument (request, response) {
     const { external_id, device_data, image, microsite, uid } = request.body
 
     const participant = await getParticipantApi(uid)
-    const userUuid = await stringToUUID(uid)
+    const userUuid = uid
     device_data.user_uuid = userUuid
 
     const [document] = await Promise.all([veryfiClient.process_document_base64string(image, null, null, false, {
